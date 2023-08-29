ae83cfb2377d72dc7ae051d746cec641eb1182ec
Yago Alencar <antonioyagoalencar@gmail.com>
2023-08-29 00:39:28 -0300

    Layout principal terminado

---
+++ src/script.js
@@ -0,0 +1,4 @@
+let regras = document.getElementById('rules')
+const pedra = document.getElementById('pedra')
+const papel = document.getElementById('papel')
+const tesoura = document.getElementById('tesoura')
