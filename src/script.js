a2a3d5a614f9cbfb3bfbd66b0868b9a228dd5437
Yago Alencar <antonioyagoalencar@gmail.com>
2023-08-30 17:01:30 -0300

    É possível jogar. Só não dá pra reiniciar ainda

--- src/script.js
+++ src/script.js
@@ -1,6 +1,3 @@
-let pedra = document.getElementById('pedra')
-let papel = document.getElementById('papel')
-let tesoura = document.getElementById('tesoura')
 let container = document.getElementById('container_inferior')
 let conteudo = document.getElementById('conteudo_inferior')
 let opcao1
@@ -8,37 +5,100 @@ let textoOpcao1 = document.createElement('p')
 let textoOpcao2 = document.createElement('p')
 let opcao2
 let conteudoNovo = document.createElement('div')
+let resultado = document.createElement('p')
+let button = document.createElement('button')
+let esquerda = document.createElement('div')
+let centro = document.createElement('div')
+let direita = document.createElement('div')
+let pedra = document.getElementById('pedra')
+let papel = document.getElementById('papel')
+let tesoura = document.getElementById('tesoura')
+//Clonando elementos para manipulá-los e conservar propriedades originais
+let clonepedra = pedra.cloneNode(true)
+let clonepapel = papel.cloneNode(true)
+let clonetesoura = tesoura.cloneNode(true)
 
-textoOpcao1.textContent = 'Your Pick'
+let idopcao1
+let idopcao2
+
+textoOpcao1.textContent = 'YOU PICK'
 textoOpcao1.className = 'te'
 textoOpcao1.id = 'texto1'
-textoOpcao2.textContent = 'House Pick'
+textoOpcao2.textContent = 'HOUSE PICK'
 textoOpcao2.className = 'te'
 textoOpcao2.id = 'texto2'
 conteudoNovo.id = 'conteudoNovo'
+resultado.className = 'resultado'
+button.id = 'button'
+button.textContent = 'PLAY AGAIN'
+centro.className = 'centro'
+direita.className = 'direita'
+esquerda.className = 'esquerda'
 
-opcao2 = pedra
+
+
+
+  function sorteio(){
+    let num = Math.floor(Math.random()*(4-1)+1)
+    if(num === 1){
+      opcao2 = clonepedra
+    }
+    else if(num === 2){
+      opcao2 = clonepapel
+    }
+    else opcao2 = clonetesoura
+
+  }
+
+function check(){
+  if(opcao1.id == opcao2.id){
+    resultado.textContent = 'DRAWN'
+  }
+  else if((opcao1.id == 'pedra' && opcao2.id == 'tesoura')||(opcao1.id == 'tesoura' && opcao2.id == 'papel')||
+  (opcao1.id == 'papel' && opcao2.id == 'pedra')){
+    resultado.textContent = 'YOU WIN'
+  }
+  else resultado.textContent = 'YOU LOSE'
+}
 
 pedra.addEventListener('click',()=>{
   opcao1 = pedra
   escolha()
+  check()
 })
+
+
+
 papel.addEventListener('click',()=>{
   opcao1 = papel
   escolha()
+  check()
 })
 tesoura.addEventListener('click',()=>{
   opcao1 = tesoura
-  opcao1.className = 'item opcao1'
   escolha()
+  check()
 })
 
 
-function escolha(){
-  container.removeChild(conteudo)
-  conteudoNovo.append(textoOpcao1,textoOpcao2,opcao1,opcao2)
-  container.appendChild(conteudoNovo)
 
+function escolha(){
+  sorteio()
+  container.removeChild(conteudo)
+  esquerda.append(textoOpcao1,opcao1)
+  centro.append(resultado,button)
+  direita.append(textoOpcao2,opcao2)
+  conteudoNovo.append(esquerda,centro,direita)
+  container.appendChild(conteudoNovo)
 }
 
- 
+ button.addEventListener('click',()=>{
+  resultado.textContent = ''
+  esquerda.innerHTML = ''
+  centro.innerHTML = ''
+  direita.innerHTML = ''
+  container.innerHTML = ''
+  opcao1 = null
+  container.append(conteudo)
+  
+})
